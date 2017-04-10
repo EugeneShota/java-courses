c81c1433b9b7d735ec0c2999e4367c3b1b12be24
EugeneShota <ewgenii996@gmail.com>
2017-04-10 20:05:21 +0400

    Calculate first

--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,16 @@ public class Calculate{
 
 	public static void main(String[] arg) {
 		System.out.println("Calculate...");
+		int first =Integer.valueOf(arg[0]);
+		int second=Integer.valueOf(arg[1]);
+		int summ=first + second;
+		System.out.println("Sum "+summ);
+		int sub=first - second;
+		System.out.println("Sub "+sub);
+		double div =first / second;
+		System.out.println("Div "+div);
+		int multi=first * second;
+		System.out.println("Multi "+multi);
 	}
 
 }
