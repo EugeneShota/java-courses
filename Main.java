6823b0ffc6aec2e475ba015219a0250ca40025fe
EugeneShota <ewgenii996@gmail.com>
2017-04-12 15:35:57 +0400

    ideaTest

---
+++ Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Евгений on 11.04.2017.
+ */
+public class Main {
+
+    public static  void  main(String [] arg) {
+    System.out.println("Hello world!!!");
+    int age=20;
+    System.out.println(age);
+    String myName = "Eugene";
+    int myWeight=90;
+    String outString=myName + "and My Age " + age + " and My weight " + myWeight;
+    System.out.println(outString);
+    }
+
+}
